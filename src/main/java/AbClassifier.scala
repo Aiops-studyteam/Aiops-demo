0ae0283cc42884a978eedbf554e4c2e763bd83fb
WillSmisi <764947976@11.com>
2018-07-11 17:32:17 +0800

    Finish the test which is about the function save model and read model

--- src/main/java/AbClassifier.scala
+++ src/main/java/AbClassifier.scala
@@ -1,6 +1,6 @@
 package com.Harmonycloud.NWClassifier
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.{MultilayerPerceptronClassificationModel, MultilayerPerceptronClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, Word2Vec}
 import org.apache.spark.sql.SQLContext
@@ -9,67 +9,154 @@ import org.apache.spark.{SparkConf, SparkContext}
 object AbClassifier {
   final val VECTOR_SIZE = 100
   def main(args: Array[String]) {
-    if (args.length < 1) {
-      println("Usage:SMSClassifier SMSTextFile")
+    if (args.length < 2) {
+      println("Usage:mode(train/test) File-Path")
       sys.exit(1)
     }
     //LogUtils.setDefaultLogLevel()
+
 
     val conf = new SparkConf().setMaster("local").setAppName("SMS Message Classification (HAM or SPAM)")
 
     //val conf = new SparkConf().setAppName("SMS Message Classification (HAM or SPAM)")
     val sc = new SparkContext(conf)
     val sqlCtx = new SQLContext(sc)
-    val parsedRDD = sc.textFile(args(0)).map(_.split("\t")).map(eachRow => {
-      (eachRow(0),eachRow(1).split(" "))
-    })
-    val msgDF = sqlCtx.createDataFrame(parsedRDD).toDF("label","message")
 
-    val labelIndexer = new StringIndexer()
-      .setInputCol("label")
-      .setOutputCol("indexedLabel")
-      .fit(msgDF)
+    if(args(0)=="train") {
+        train()
+      }
+    else if(args(0)=="test"){
+        test()
+    }
+    else{
+      println("Usage:mode(train/test) File-Path")
+      sys.exit(1)
+    }
 
-    val word2Vec = new Word2Vec()
-      .setInputCol("message")
-      .setOutputCol("features")
-      .setVectorSize(VECTOR_SIZE)
-      .setMinCount(1)
 
-    val layers = Array[Int](VECTOR_SIZE,6,5,2)
-    val mlpc = new MultilayerPerceptronClassifier()
-      .setLayers(layers)
-      .setBlockSize(512)
-      .setSeed(1234L)
-      .setMaxIter(128)
-      .setFeaturesCol("features")
-      .setLabelCol("indexedLabel")
-      .setPredictionCol("prediction")
 
-    val labelConverter = new IndexToString()
-      .setInputCol("prediction")
-      .setOutputCol("predictedLabel")
-      .setLabels(labelIndexer.labels)
 
-    val Array(trainingData, testData) = msgDF.randomSplit(Array(0.8, 0.2))
 
-    val pipeline = new Pipeline().setStages(Array(labelIndexer,word2Vec,mlpc,labelConverter))
-    val model = pipeline.fit(trainingData)
 
-    val predictionResultDF = model.transform(testData)
-    //below 2 lines are for debug use
-    predictionResultDF.printSchema
-    predictionResultDF.select("message","label","predictedLabel").show(30)
 
-    val evaluator = new MulticlassClassificationEvaluator()
-      .setLabelCol("indexedLabel")
-      .setPredictionCol("prediction")
-      //  .setMetricName("precision")
-      //Since SPARK-15617 deprecated precision in MulticlassClassificationEvaluator, many ML examples broken.
-      //We should use accuracy to replace precision in these examples.
-      .setMetricName("accuracy")
-    val predictionAccuracy = evaluator.evaluate(predictionResultDF)
-    println("Testing Accuracy is %2.4f".format(predictionAccuracy * 100) + "%")
-    sc.stop
+
+
+
+
+
+
+
+    //test function
+    def test():Unit = {
+      println("Test function")
+      val parsedRDD = sc.textFile(args(1)).map(_.split("\t")).map(eachRow => {
+        (eachRow(0),eachRow(1).split(" "))
+      })
+      val msgDF = sqlCtx.createDataFrame(parsedRDD).toDF("label","message")
+
+      val labelIndexer = new StringIndexer()
+        .setInputCol("label")
+        .setOutputCol("indexedLabel")
+        .fit(msgDF)
+
+      val Array(trainingData, testData) = msgDF.randomSplit(Array(0.8, 0.2))
+      val layers = Array[Int](VECTOR_SIZE,6,5,2)
+      val mlpc = new MultilayerPerceptronClassifier()
+        .setLayers(layers)
+        .setBlockSize(512)
+        .setSeed(1234L)
+        .setMaxIter(128)
+        .setFeaturesCol("features")
+        .setLabelCol("indexedLabel")
+        .setPredictionCol("prediction")
+
+      val model=PipelineModel.load("D:\\Harmonycloud\\AIOpsNW\\model\\CNN_model.model")
+      val predictionResultDF = model.transform(testData)
+      //below 2 lines are for debug use
+      predictionResultDF.printSchema
+      predictionResultDF.select("message","label","predictedLabel").show(30)
+
+      val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("indexedLabel")
+        .setPredictionCol("prediction")
+        //  .setMetricName("precision")
+        //Since SPARK-15617 deprecated precision in MulticlassClassificationEvaluator, many ML examples broken.
+        //We should use accuracy to replace precision in these examples.
+        .setMetricName("accuracy")
+      val predictionAccuracy = evaluator.evaluate(predictionResultDF)
+      println("Testing Accuracy is %2.4f".format(predictionAccuracy * 100) + "%")
+        sc.stop
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //train function
+    def train(): Unit = {
+
+      val parsedRDD = sc.textFile(args(1)).map(_.split("\t")).map(eachRow => {
+        (eachRow(0),eachRow(1).split(" "))
+      })
+      val msgDF = sqlCtx.createDataFrame(parsedRDD).toDF("label","message")
+
+      val labelIndexer = new StringIndexer()
+        .setInputCol("label")
+        .setOutputCol("indexedLabel")
+        .fit(msgDF)
+
+      val word2Vec = new Word2Vec()
+        .setInputCol("message")
+        .setOutputCol("features")
+        .setVectorSize(VECTOR_SIZE)
+        .setMinCount(1)
+
+      val layers = Array[Int](VECTOR_SIZE,6,5,2)
+      val mlpc = new MultilayerPerceptronClassifier()
+        .setLayers(layers)
+        .setBlockSize(512)
+        .setSeed(1234L)
+        .setMaxIter(128)
+        .setFeaturesCol("features")
+        .setLabelCol("indexedLabel")
+        .setPredictionCol("prediction")
+
+      val labelConverter = new IndexToString()
+        .setInputCol("prediction")
+        .setOutputCol("predictedLabel")
+        .setLabels(labelIndexer.labels)
+
+      val Array(trainingData, testData) = msgDF.randomSplit(Array(0.8, 0.2))
+
+      val pipeline = new Pipeline().setStages(Array(labelIndexer,word2Vec,mlpc,labelConverter))
+      val model = pipeline.fit(trainingData)
+
+      model.save("D:\\Harmonycloud\\AIOpsNW\\model\\CNN_model.model")
+      /*val predictionResultDF = model.transform(testData)
+      //below 2 lines are for debug use
+      predictionResultDF.printSchema
+      predictionResultDF.select("message","label","predictedLabel").show(30)
+
+      val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("indexedLabel")
+        .setPredictionCol("prediction")
+        //  .setMetricName("precision")
+        //Since SPARK-15617 deprecated precision in MulticlassClassificationEvaluator, many ML examples broken.
+        //We should use accuracy to replace precision in these examples.
+        .setMetricName("accuracy")
+      val predictionAccuracy = evaluator.evaluate(predictionResultDF)
+      println("Testing Accuracy is %2.4f".format(predictionAccuracy * 100) + "%")*/
+      sc.stop
+    }
+
+
+
   }
 }
